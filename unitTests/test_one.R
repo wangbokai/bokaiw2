edc2c5ea34c0fcf342be1afe0e3770f7bd74b1b9
wangbokai <wang_bokai@hotmail.com>
2015-09-27 16:19:18 -0400

    Revert "added a unit test"
    This reverts commit 8ade401a4f52439315acf9fc08aa3c7579e94b46.

--- unitTests/test_one.R
+++
@@ -1,8 +0,0 @@
-## library('RUnit')
-## check to see if the total probabilities sum up to 1
-
-## for example, p.vec = c(1/2, 1/4, 1/4, 0)
-test.equal.one <- function()
-{
-  checkEquals(sum(p.vec), 1)
-}
