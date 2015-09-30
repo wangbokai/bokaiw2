51535ad0553d0036a4debe1e559373d06a1eff13
wangbokai <wang_bokai@hotmail.com>
2015-09-30 15:09:23 -0400

    adding

--- R/simplebgwprocess.R
+++ R/simplebgwprocess.R
@@ -5,7 +5,7 @@
 ## n.sim is the number of independent simulations in each case
 ## below is the function defined for simulating samples from a simple (BGW) Branching process
 
-bokai.bgw.process = function(n, p.vec, n.sim) {
+simplebgwprocess = function(n, p.vec, n.sim) {
   ## x.mat is the matrix to store all Xn vectors
   x.mat = matrix(data=rep(0, (n+1)*n.sim), nrow=n.sim, ncol=n+1)
 
@@ -27,4 +27,4 @@ bokai.bgw.process = function(n, p.vec, n.sim) {
 }
 
 ## x.sample is the generated samples of this (BGW) Branching process
-## x.sample = bokai.bgw.process(n=10, p.vec=c(1/2, 1/4, 1/4, 0), n.sim=100)
+## x.sample = simplebgwprocess(n=10, p.vec=c(1/2, 1/4, 1/4, 0), n.sim=100)
